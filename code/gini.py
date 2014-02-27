269b1aa471a2818caf297b779a2ec0423617af2a
cjnayak <chayajnayak@gmail.com>
2014-02-27 01:20:10 -0800

    Finished HW5

--- code/gini.py
+++ code/gini.py
@@ -5,6 +5,8 @@ Classes are strings."""
 
 import fileinput
 import csv
+import json
+from collections import defaultdict, Counter
 
 (
     CMTE_ID, AMNDT_IND, RPT_TP, TRANSACTION_PGI, IMAGE_NUM, TRANSACTION_TP,
@@ -20,23 +22,43 @@ CANDIDATES = {
 
 ############### Set up variables
 # TODO: declare datastructures
+candidates_two= defaultdict(int)
+count = 0.0
+cand = defaultdict(lambda: defaultdict(int))
 
 ############### Read through files
-for row in csv.reader(fileinput.input(), delimiter='|'):
+
+for row in csv.reader(fileinput.input(files=('itpas2.txt')), delimiter='|'):
     candidate_id = row[CAND_ID]
     if candidate_id not in CANDIDATES:
         continue
-
     candidate_name = CANDIDATES[candidate_id]
     zip_code = row[ZIP_CODE]
+    count += 1
+    candidates_two[candidate_name] += 1
+    cand[zip_code][candidate_name] += 1
+
     ###
     # TODO: save information to calculate Gini Index
     ##/
 
+gini_final_zip = []
+for i in cand:
+    donate_zip = float(sum(cand[i][j] for j in cand[i]))
+
+    cand_zip = []
+    for j in cand[i]:
+        gini_one = (cand[i][j] / donate_zip)**2
+        cand_zip.append(gini_one)
+
+    score_with_weight = (1 - sum(cand_zip)) * (donate_zip / count)
+    gini_final_zip.append(score_with_weight)
+
+
 ###
 # TODO: calculate the values below:
-gini = 0  # current Gini Index using candidate name as the class
-split_gini = 0  # weighted average of the Gini Indexes using candidate names, split up by zip code
+gini = 1 - sum((candidates_two[j] / count)** 2 for j in candidates_two)  # current Gini Index using candidate name as the class
+split_gini = sum(gini_final_zip)  # weighted average of the Gini Indexes using candidate names, split up by zip code
 ##/
 
 print "Gini Index: %s" % gini
